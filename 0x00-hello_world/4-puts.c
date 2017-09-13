c07a1d5f26d5adda3f9b30ad874756e3ab77a5d5
Yunju Chen <info@yunjuchen.net>
2017-09-13 21:31:02 +0000

    Correct doc and style issues

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
+/**
+ * main - puts function
+ *
+ * Description: This function prints a line of quote
+ * Return: 0
+ */
+
 #include <stdio.h>
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }

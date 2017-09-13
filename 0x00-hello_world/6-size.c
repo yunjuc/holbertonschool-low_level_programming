baea3d06a323f6d564efa51e27dbd21d475d246d
Yunju Chen <info@yunjuchen.net>
2017-09-13 22:23:14 +0000

    First commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+ /**
+ * main - Print size of variables
+ *
+ * Description: This function prints size of different variables in the current computer
+ * Return: 0
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	char c;
+	int i;
+	long int lg_i;
+	long long int lg2_i;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(lg_i));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lg2_i));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
+}

78f517b479c5bca3ca725e8da2b38a0f1b277902
Yunju Chen <info@yunjuchen.net>
2017-09-13 21:49:27 +0000

    First commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+/**
+ * main - printf function
+ *
+ * Description: This function prints a line of sentence
+ * Return: 0
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

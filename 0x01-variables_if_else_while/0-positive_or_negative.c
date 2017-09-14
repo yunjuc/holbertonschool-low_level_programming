49bd30ce5d36b1ae9750f36050e551c9780026ee
Yunju Chen <info@yunjuchen.net>
2017-09-14 21:43:37 +0000

    replace = with ==

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,16 +15,13 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n >= 0)
+	if (n > 0)
 	{
-		if (n > 0)
-		{
-			printf("%d is positive\n", n);
-		}
-		else
-		{
-			printf("%d is 0\n", n);
-		}
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is 0\n", n);
 	}
 	else
 	{

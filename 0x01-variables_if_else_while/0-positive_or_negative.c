daeb9aebb7a002bf120cdfc269e1da447ef4d1bf
Yunju Chen <info@yunjuchen.net>
2017-09-14 21:49:19 +0000

    Remove {}

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,17 +16,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-	{
 		printf("%d is positive\n", n);
-	}
 	else if (n == 0)
-	{
 		printf("%d is 0\n", n);
-	}
 	else
-	{
 		printf("%d is negative\n", n);
-	}
 
 	return (0);
 }

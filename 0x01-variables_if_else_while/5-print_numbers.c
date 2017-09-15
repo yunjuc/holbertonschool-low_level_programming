b26f245f703b5f37d919341b85276ed0eb777f64
Yunju Chen <info@yunjuchen.net>
2017-09-15 00:06:57 +0000

    First commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Numbers
+ * Description: Print numbers 0-10
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n == 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	putchar('\n');
+	return (0);
+}

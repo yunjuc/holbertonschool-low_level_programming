b3c2f2167902e9c0977651afeea8c8c3435843d6
Yunju Chen <info@yunjuchen.net>
2017-09-15 04:50:31 +0000

    First commit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 
+/**
+ * main - Numberz
+ * Description: Print numbers using putchar
+ * Return: 0
+ */
+
 int main(void)
 {
 	int n;
-	
-	n = '0';
+
+	for (n = '0'; n <= '9'; n++)
 	putchar(n);
-	
+
+	putchar('\n');
 	return (0);
 }

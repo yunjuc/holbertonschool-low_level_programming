27188f818367a2f58e2d22da617615cf867b3127
Yunju Chen <info@yunjuchen.net>
2017-09-15 23:24:48 +0000

    First commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Comb5
+ * Description: Print 3 digits numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, j, t;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = i + 1; j <= '9'; j++)
+		{
+			for (t = i + 2; t != i && t != j && t <= '9'; t++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(t);
+				if (i == '7' && j == '8' && t == '9')
+					continue;
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar ('\n');
+	return (0);
+}

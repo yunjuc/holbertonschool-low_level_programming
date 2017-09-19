36adb0f6476576c383f4ce16f342475fbb7f1c95
Yunju Chen <info@yunjuchen.net>
2017-09-19 15:31:46 +0000

    First commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _abs - print absolute value
+ * @c: integer to be checked
+ *
+ * Description: check the absolute value of an integer
+ * Return: c - if c is greater than 0
+ *         0 - if c is 0
+ *         (0-c) - if c is smaller than 0
+ */
+int _abs(int c)
+{
+	if (c > 0)
+		return (c);
+	else if (c == 0)
+		return (0);
+	else
+		return (0 - c);
+}

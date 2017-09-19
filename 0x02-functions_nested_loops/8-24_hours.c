bb2de057d07472bbdb895b5997142d79afddca11
Yunju Chen <info@yunjuchen.net>
2017-09-19 20:56:26 +0000

    add if..esle to stop at '23'

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,55 +8,34 @@ void jack_bauer(void)
 	int i, j, t, n;
 
 	i = '0';
-
 	while (i <= '2')
-	{
+	{	
 		j = '0';
 		while (j <= '9')
 		{
-			t = '0';
-			while (t < '6')
+			if (i == '2' && j == '4')
+				break;
+			else
 			{
-				n = '0';
-				while (n <= '9')
+				t = '0';
+				while (t < '6')
 				{
-					_putchar(i);
-					_putchar(j);
-					_putchar(':');
-					_putchar(t);
-					_putchar(n);
-					_putchar('\n');
-					n++;
+					n = '0';
+					while (n <= '9')
+					{
+						_putchar(i);
+						_putchar(j);
+						_putchar(':');
+						_putchar(t);
+						_putchar(n);
+						_putchar('\n');
+						n++;
+					}
+					t++;
 				}
-				t++;
-			}
-			j++;
+				j++;
+			}	
 		}
 		i++;
 	}
-
-	i = '2';
-	j = '0';
-
-	while (j < '4')
-	 {
-		t = '0';
-		while (t < '6')
-		{
-			n = '0';
-			while (n <= '9')
-			{
-				_putchar(i);
-				_putchar(j);
-				_putchar(':');
-				_putchar(t);
-				_putchar(n);
-				_putchar('\n');
-				n++;
-			}
-			t++;
-		}
-		j++;
-	}
-
 }

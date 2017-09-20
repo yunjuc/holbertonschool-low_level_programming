bb88b70a6d764e1acda35f99fbe6f69bdb6ac521
Yunju Chen <info@yunjuchen.net>
2017-09-20 22:34:46 +0000

    First commit

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,7 +3,33 @@
 /**
  * main - sum of nature numbers
  *
- * Description: comput the sum of multiples of 3 and 5 below 1024
+ * Description: compute the sum of multiples of 3 and 5 below 1024
  * Return: 0
  */
- 
+int main(void)
+{
+	int i, num, sum;
+
+	sum = 0;
+	num = 0;
+	for (i = 0; i < 1024 / 3; i++)
+	{
+		num = num + 3;
+		if (num < 1024)
+			sum = sum + num;
+	}
+	num = 0;
+	for (i = 0; i < 1024 / 5; i++)
+	{
+		num = num + 5;
+		if (num < 1024)
+			sum = sum + num;
+	}
+	num = 0;
+	for (i = 0; i < 1024 / 15; i++)
+		num = num + 5;
+		if (num < 1024)
+			sum = sum -num; 
+	printf("%d\n", sum);
+	return (0);
+} 

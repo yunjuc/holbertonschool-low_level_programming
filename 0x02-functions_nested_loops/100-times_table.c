f33c7bd81c6b645e65aaa3c9ce0cfa02241b718b
Yunju Chen <info@yunjuchen.net>
2017-09-20 20:48:49 +0000

    First commit

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+
+/**
+ * print_times_table - print times table 15 times
+ * @c: number of time to print
+ *
+ * Description: take a number n and print n*n times table
+ */
+void print_times_table(int c)
+{
+	int i, j, product;
+
+	if (c < 15 && c >= 0)
+	{
+		for (i = 0; i <= c; i++)
+		{
+			for (j = 0; j <= c; j++)
+			{
+				product = i * j;
+
+				if (j == 0)
+					_putchar('0');
+				else
+				{
+		 			if (product / 100 == 0)
+						_putchar(' ');
+					else
+						_putchar(product / 100 + '0');
+					if (product / 100 == 0 && product / 10 % 10 == 0)
+						_putchar(' ');
+					else
+						_putchar(product / 10 % 10 + '0');
+					_putchar(product % 10 + '0');
+				}
+				if (j == c)
+					;
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	return ;
+}
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -8,12 +8,12 @@
 
 int main(void)
 {
-    int r;
-
-    print_last_digit(98);
-    print_last_digit(0);
-    r = print_last_digit(-1024);
-    _putchar('0' + r);
+    print_times_table(3);
     _putchar('\n');
+    print_times_table(5);
+    _putchar('\n');
+    print_times_table(98);
+    _putchar('\n');
+    print_times_table(12);  
     return (0);
 }

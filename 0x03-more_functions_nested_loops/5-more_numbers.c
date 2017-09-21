c94948a7b511c32a378854a899c30e5017135b72
Yunju Chen <info@yunjuchen.net>
2017-09-21 20:56:29 +0000

    fix style issues

--- 0x03-more_functions_nested_loops/5-more_numbers.c
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -11,8 +11,8 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j /10 == 0)
-				_putchar (j %10 + '0');
+			if (j / 10 == 0)
+				_putchar (j % 10 + '0');
 			else
 			{
 				_putchar(j / 10 + '0');

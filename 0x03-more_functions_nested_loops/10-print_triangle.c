3b455423f0c869cd5b07c5bbfe381830ccc8d0f2
Yunju Chen <info@yunjuchen.net>
2017-09-22 04:03:36 +0000

    #10 first commit

--- 0x03-more_functions_nested_loops/10-print_triangle.c
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -2,20 +2,29 @@
 
 /**
  * print_triangle - print triagle
- * @size - size of triangle
+ * @size: size of triangle
+ *
+ * Description: print triangle the size of @size input
  */
 void print_triangle(int size)
 {
-	int i, j, t;	
+	int i, j, t;
 
 	if (size <= 0)
 		_putchar('\n');
 	else
 	{
-		for (i = 0, i <= size, i ++)
+		for (i = 0; i < size; i++)
 		{
-			_putchar(' ');
+			for (j = 0; j < size - i - 1; j++)
+			{
+				_putchar(' ');
+			}
+			for (t = 0; t <= i ; t++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
-		for (j = 0
 	}
 }

3c407647e55fc0221e787680fc4877e9bfe7676d
Yunju Chen <info@yunjuchen.net>
2017-09-25 22:12:35 +0000

    #0 first commit

--- 0x04-pointers_arrays_strings/0-reset_to_98.c
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 
 /**
- * void reset_to_98(int *n);
- * @*n: address of int n
+ * reset_to_98 - use pointer to change int value
+ * @n: address of int n
  *
  * Description: take an int pointer and change the value
  */
 void reset_to_98(int *n)
 {
-	
+	*n = 98;
 }

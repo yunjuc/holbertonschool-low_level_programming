a6f0fc31b3b8ac2f523aeda1f6e316200118fcdb
Yunju Chen <info@yunjuchen.net>
2017-09-26 17:16:49 +0000

    #4 first commit

--- 0x04-pointers_arrays_strings/4-print_rev.c
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -9,13 +9,12 @@
  */
 void print_rev(char *s)
 {
-	int i;
+	int i, j;
 
+	i = 0;
 	while (s[i] != 0)
 		i++;
-	while (s[i] >= s[0])
-	{	_putchar(s[i]);
-		i--;
-	}
+	for (j = i - 1; j >= 0 ; j--)
+		_putchar(s[j]);
 	_putchar('\n');
 }

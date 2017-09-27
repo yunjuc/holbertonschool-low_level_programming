0522dd66b8a9a308d5e748e7bf8a79131a460c8c
Yunju Chen <info@yunjuchen.net>
2017-09-27 04:38:51 +0000

    #9 first commit

---
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * *_strcpy - copy string
+ * @dest: string name
+ * @src: string to be copied
+ *
+ * Description: copya string and point it to a pointer
+ * Return: destination pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (dest = src);
+}

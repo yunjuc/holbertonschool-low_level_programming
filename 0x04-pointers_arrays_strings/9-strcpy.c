841a3598ed8adfd011dc3963c361787681d77595
Yunju Chen <info@yunjuchen.net>
2017-09-27 21:28:07 +0000

    change code to copy string value not just pass address

--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -1,14 +1,19 @@
 #include "holberton.h"
 
 /**
- * *_strcpy - copy string
+ * _strcpy - copy string
  * @dest: string name
  * @src: string to be copied
  *
  * Description: copya string and point it to a pointer
- * Return: destination pointer
+ * R:eturn: destination pointer
  */
 char *_strcpy(char *dest, char *src)
 {
-	return (dest = src);
+	int i;
+
+	for (i = 0; src[i] != 0; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
 }
--- 0x04-pointers_arrays_strings/main.c
+++ 0x04-pointers_arrays_strings/main.c
@@ -8,10 +8,11 @@
  */
 int main(void)
 {
-    char s[10] = "Holberton";
+    char s1[98] = "test";
+    char *p;
 
-    printf("%s\n", s);
-    rev_string(s);
-    printf("%s\n", s);
+    p = _strcpy(s1, "First, solve the problem. Then, write the code\n");
+    printf("%s", s1);
+    printf("%s", p);
     return (0);
 }

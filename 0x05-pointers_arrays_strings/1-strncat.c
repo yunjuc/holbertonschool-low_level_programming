25423f7df7887491540349b8ee5956c7584defb4
Yunju Chen <info@yunjuchen.net>
2017-09-28 22:53:42 +0000

    add condition n

--- 0x05-pointers_arrays_strings/1-strncat.c
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -8,27 +8,13 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int l1, l2, t;
+	int i, j;
 
-	for (l1 = 0; dest[l1] != 0; l1++)
-		l1 = l1;
-	for (l2 = 0; src[l2] != 0; l2++)
-		l2 = l2;
-	if (n <= l2)
-	{
-		for (t = 0; t < n; t++)
-		{
-			dest[l1 + 1] = src[t];
-			l1++;
-		}
-	}
-	else
-	{
-		for (t = 0; t <= l2; t++)
-		{
-			dest[l1 + 1] = src[t];
-			l1++;
-		}
-	}
+	for (i = 0; dest[i] != 0; i++)
+		i = i;
+	for (j = 0; j < n && src[j] != 0; j++)
+		dest[i] = src[j];
+		i++;
+	dest[i] = '\0';
 	return (dest);
 }

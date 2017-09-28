921f358f906cfe2ea11594591156acfb9ba9efdc
Yunju Chen <info@yunjuchen.net>
2017-09-28 19:14:06 +0000

    remove space between strings

--- 0x05-pointers_arrays_strings/1-strncat.c
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -10,11 +10,12 @@ char *_strcat(char *dest, char *src)
 
 	for (i = 0; dest[i] != 0; i++)
 		i = i;
+	dest[i] = ' ';
 	for (j = 0; src[j] != 0; j++)
 	{
-		dest[i] = src[j];
+		dest[i + 1] = src[j];
 		i++;
 	}
-	dest[i + j] = '\0';
+	dest[i + j + 1] = '\0';
 	return (dest);
 }

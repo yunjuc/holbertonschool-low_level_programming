3365ca3a3b0f0110c654533237f48ac1de730fbd
Yunju Chen <info@yunjuchen.net>
2017-09-28 23:04:24 +0000

    add {} to for loop

--- 0x05-pointers_arrays_strings/1-strncat.c
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -13,8 +13,10 @@ char *_strncat(char *dest, char *src, int n)
 	for (i = 0; dest[i] != 0; i++)
 		i = i;
 	for (j = 0; j < n && src[j] != 0; j++)
+	{
 		dest[i] = src[j];
 		i++;
+	}
 	dest[i] = '\0';
 	return (dest);
 }

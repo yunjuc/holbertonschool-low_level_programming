3c3158b6a3b9b7a62f6f4dec3837abb394292471
Yunju Chen <info@yunjuchen.net>
2017-09-28 22:30:56 +0000

    First commit

--- 0x05-pointers_arrays_strings/1-strncat.c
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -1,21 +1,34 @@
 /**
- * _strcat - concatenat two strings
- * @dest: pointer to destination string
- * @src: pointer to source sting
+ * _strncat - concatenat two strings
+ * @dest: destination string
+ * @src: source sting
+ * @n: number of char to copy
+ * Description: copy "n" char from source string and append to dest string
  * Return: pointer to final string output
  */
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+	int l1, l2, t;
 
-	for (i = 0; dest[i] != 0; i++)
-		i = i;
-	dest[i] = ' ';
-	for (j = 0; src[j] != 0; j++)
+	for (l1 = 0; dest[l1] != 0; l1++)
+		l1 = l1;
+	for (l2 = 0; src[l2] != 0; l2++)
+		l2 = l2;
+	if (n <= l2)
 	{
-		dest[i + 1] = src[j];
-		i++;
+		for (t = 0; t < n; t++)
+		{
+			dest[l1 + 1] = src[t];
+			l1++;
+		}
 	}
-	dest[i + j + 1] = '\0';
+	else
+	{
+		for (t = 0; t <= l2; t++)
+		{
+			dest[l1 + 1] = src[t];
+			l1++;
+		}
+	}
 	return (dest);
 }

81aa4581487649a410d12cd953a25b5b1aec5fe7
Yunju Chen <info@yunjuchen.net>
2017-09-28 16:46:00 +0000

    First commit

--- 0x05-pointers_arrays_strings/0-strcat.c
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -6,5 +6,16 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	
+	int i, j;
+
+	for (i = 0; dest[i] != 0; i++)
+		i = i;
+	dest[i] = ' ';
+	for (j = 0; src[j] != 0; j++)
+	{
+		dest[i + 1] = src[j];
+		i++;
+	}
+	dest[i + j + 1] = '\0';
+	return (dest);
 }

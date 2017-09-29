cb8e10c07521d6daa972ac84731dae6c9c841c47
Yunju Chen <info@yunjuchen.net>
2017-09-29 05:53:08 +0000

    add ' ' around numbers

--- 0x05-pointers_arrays_strings/7-leet.c
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -13,15 +13,15 @@ char *leet(char *s)
 	for (j = 0; j < i; j++)
 	{
 		if (s[j] == 'a' || s[j] == 'A')
-			s[j] = 4;
+			s[j] = '4';
 		else if (s[j] == 'e' || s[j] == 'E')
-			s[j] = 3;
+			s[j] = '3';
 		else if (s[j] == 'o' || s[j] == 'O')
-			s[j] = 0;
+			s[j] = '0';
 		else if (s[j] == 't' || s[j] == 'T')
-			s[j] = 7;
+			s[j] = '7';
 		else if (s[j] == 'l' || s[j] == 'L')
-			s[j] = 1;
+			s[j] = '1';
 		else
 		{
 			;

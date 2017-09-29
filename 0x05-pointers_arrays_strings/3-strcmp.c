019c965702a070718bfedc91e7da03502139eaf9
Yunju Chen <info@yunjuchen.net>
2017-09-29 02:37:50 +0000

    first commit

---
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+/**
+ * _strcmp - compare two strings
+ * @s1: 1st string
+ * @s2: 2nd sting
+ * Description: compare the first char between 2 strings and return difference
+ * Return: the differece in intger
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = s1[0] - s2[0];
+	return (i);
+}

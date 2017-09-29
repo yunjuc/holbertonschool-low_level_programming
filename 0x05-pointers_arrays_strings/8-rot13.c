de10f424936295219edc51bc151c3c4adf428683
Yunju Chen <info@yunjuchen.net>
2017-09-29 05:24:20 +0000

    first commit

---
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,25 @@
+/**
+ * rot13 - encode a string
+ * @s: string of be checked
+ * Description: take a string and encode it with rot13
+ * Return: encoded string
+ */
+char *rot13(char *s)
+{
+	int i, j;
+
+	for (i = 0; s[i] != 0; i++)
+		i = i;
+	for (j = 0; j < i; j++)
+	{
+		if (s[j])
+
+		else if (s[j])
+
+		else
+		{
+			;
+		}
+	}
+	return (s);
+}

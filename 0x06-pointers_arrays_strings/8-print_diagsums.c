808d431dfec813be09749963b78b08c833b598fc
Yunju Chen <info@yunjuchen.net>
2017-10-03 07:40:01 +0000

    initialize sum2

--- 0x06-pointers_arrays_strings/8-print_diagsums.c
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -9,11 +9,13 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, sum1, sum2;
+	int i, j, sum1, sum2;
 
+	sum1 = 0;
 	for (i = 0; i < size * size ; i += size + 1)
 		sum1 += *(a + i);
-	for (i = size - 1; i < size * size ; i += size - 1)
-		sum2 += *(a + i);
+	sum2 = 0;
+	for (j = size - 1; j < size * (size - 1) + 1 ; j += size - 1)
+		sum2 += *(a + j);
 	printf("%d, %d\n", sum1, sum2);
 }

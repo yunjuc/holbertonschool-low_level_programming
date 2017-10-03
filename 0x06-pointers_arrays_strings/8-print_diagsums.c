2312f2d621416f3a79241097d5d89b7d10ac1c30
Yunju Chen <info@yunjuchen.net>
2017-10-03 04:07:14 +0000

    first commit

--- 0x06-pointers_arrays_strings/8-print_diagsums.c
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -2,19 +2,18 @@
 #include <stdio.h>
 
 /**
- * prnt-diagsums - print sums of diagonal integers of a matrix
+ * print_diagsums - print sums of diagonal integers of a matrix
  * @a: row of matrix
  * @size: column of matrix
  * Return: 0
  */
-void print_diagsums(int *a int size)
+void print_diagsums(int *a, int size)
 {
-	int i, j;
+	int i, j, sum1, sum2;
 
-	for (i = 0 ; i < 8 ; i++)
-	{
-		for (j = 0; j < 8; j++)
-			_putchar(a[i][j]);
-		_putchar('\n');
-	}
+	for (i = 0, j = 0 ; i < size && j < size ; i++, j++)
+		sum1 += a[i][j];
+	for (i = size, j = size; i > 0 && j > 0 ; i--, j--)
+		sum2 += a[i][j];
+	printf("%d, %d", sum1, sum2);
 }

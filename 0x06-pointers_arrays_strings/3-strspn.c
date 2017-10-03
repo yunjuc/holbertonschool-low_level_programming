3e4c06a1191112f76887151748e4ea6da6322f70
Yunju Chen <info@yunjuchen.net>
2017-10-03 21:28:24 +0000

    first commit

--- 0x06-pointers_arrays_strings/3-strspn.c
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -4,15 +4,20 @@
  * _strspn - get lengths of prefix substring
  * @s: source string
  * @accept: substring to compare
- * Return: number ot byte found in source string 
+ * Return: number ot byte found in source string
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i;
+	unsigned int count = 0;
+	int i;
 
-	for ( ; *s != 0; s++)
+	for ( ; *s != '\0'; s++)
 	{
-		for ( ; *accept != 0; aceept++)
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (accept[i] == *s)
+				count++;
+		}
 	}
-	return (i);
+	return (count);
 }

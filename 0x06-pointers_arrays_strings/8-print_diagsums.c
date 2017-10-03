e2a32795499a1e243d210b6d24e04c06e5340170
Yunju Chen <info@yunjuchen.net>
2017-10-03 04:11:30 +0000

    use *a replace a[i][j]

--- 0x06-pointers_arrays_strings/8-print_diagsums.c
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -9,11 +9,11 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j, sum1, sum2;
+	int sum1, sum2;
 
-	for (i = 0, j = 0 ; i < size && j < size ; i++, j++)
-		sum1 += a[i][j];
-	for (i = size, j = size; i > 0 && j > 0 ; i--, j--)
-		sum2 += a[i][j];
-	printf("%d, %d", sum1, sum2);
+	for (*a = 0 ; *a < size ; a++)
+		sum1 += *a;
+	for (*a = size; *a > 0 ; a--)
+		sum2 += *a;
+	printf("%d, %d\n", sum1, sum2);
 }

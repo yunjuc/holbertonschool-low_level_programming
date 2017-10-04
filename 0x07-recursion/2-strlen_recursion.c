34d9e5d130a6092c8d045961ff203286245ddd21
Yunju Chen <info@yunjuchen.net>
2017-10-04 23:27:15 +0000

    first commit

---
+++ 0x07-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - return length of a string
+ * @s: pointer to source string
+ * Return: string length
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}

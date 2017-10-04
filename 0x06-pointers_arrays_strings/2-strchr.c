76a3578d72f289c94d7fcf06ece4cd931d2dc4c5
Yunju Chen <info@yunjuchen.net>
2017-10-04 06:19:32 +0000

    change to *s notation

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -5,20 +5,17 @@
  * _strchr - search char in a string
  * @s: string
  * @c: char to be searched
- * Return: char - first occurrence when char is found
+ * Return: s - first occurrence when char is found
  *         NULL - char is not found
  */
 char *_strchr(char *s, char c)
 {
-	int i;
-
 	if (c == '\0')
 		return (s);
-	for (i = 0; s[i] != '\0'; )
+	for ( ; *s != '\0'; s++)
 	{
-		if (s[i] == c)
-			return (&s[i]);
-		i++;
+		if (*s == c)
+			return (s);
 	}
 	return (NULL);
 }

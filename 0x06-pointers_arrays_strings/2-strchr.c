77e1fe31cde3092e2f6f16045a603a32ee589d57
Yunju Chen <info@yunjuchen.net>
2017-10-04 05:12:50 +0000

    check if c is null

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -12,12 +12,12 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
+	if (c == '\0')
+		return (s);
 	for (i = 0; s[i] != 0; i++)
 	{
 		if (s[i] == c)
 			return (&s[i]);
 	}
-	if (c == '\0')
-		return (NULL);
 	return (NULL);
 }

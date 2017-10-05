d48a3c1e096ad323bbc211f95c7175440aa2f8d7
Yunju Chen <info@yunjuchen.net>
2017-10-05 01:19:16 +0000

    first commit

---
+++ 0x07-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - return x to the power of y
+ * @x: base number
+ * @y: times of power
+ * Return: x to the power of y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (x == 0)
+		return (0);
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	if (y == 1)
+		return (x);
+	return (x * _pow_recursion(x, y - 1));
+}

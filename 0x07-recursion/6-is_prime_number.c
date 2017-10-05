1414792d348879acba6a7f74eea321c329cb0f12
Yunju Chen <info@yunjuchen.net>
2017-10-05 06:20:37 +0000

    first commit

---
+++ 0x07-recursion/6-is_prime_number.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * _is_prime_number - find if an int a prime number
+ * @n: number to be checked
+ * Return: 1 - is prime number
+ *         0 - not prime number
+ */
+int _is_prime_number(int n)
+{
+	
+}

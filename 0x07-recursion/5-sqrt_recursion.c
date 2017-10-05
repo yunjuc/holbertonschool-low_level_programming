1ce599401642ca88f4c54d0b2e2a85c508cf127d
Yunju Chen <info@yunjuchen.net>
2017-10-05 06:21:15 +0000

    first commit

---
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * _sqrt_recursion - return square root of a number
+ * @n: number to be chcked
+ * Return: square root of n
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return ();
+}

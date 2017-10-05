27f6c99c3cb520da549ad63f296c4862085068df
Yunju Chen <info@yunjuchen.net>
2017-10-05 23:59:09 +0000

    2nd commit

--- 0x07-recursion/6-is_prime_number.c
+++ 0x07-recursion/6-is_prime_number.c
@@ -1,12 +1,36 @@
 #include "holberton.h"
 
+int checker(int, int);
+
 /**
- * _is_prime_number - find if an int a prime number
+ * is_prime_number - find if an int a prime number
  * @n: number to be checked
  * Return: 1 - is prime number
  *         0 - not prime number
  */
-int _is_prime_number(int n)
+int is_prime_number(int n)
 {
-	
+	int i = 1;
+
+	if (n <= 1)
+		return (0);
+	else if (checker(n, i) == 0)
+		return (0);
+	return (n / checker(n, i));
+}
+
+/**
+ * checker - locate prime number
+ * @n: number to be checked
+ * @i: potential prime factor
+ * Return: 1 - is prime number
+ *         0 - not prime number
+ */
+int checker(int n, int i)
+{
+	if (n == i)
+		return (i);
+	else if (n % i == 0)
+		return (0);
+	return (checker(n, i + 1));
 }

16083882105c9ac9296a4616cede1a64e0298c1a
Yunju Chen <info@yunjuchen.net>
2017-10-05 06:46:54 +0000

    2nd commit

--- 0x07-recursion/1-print_rev_recursion.c
+++ 0x07-recursion/1-print_rev_recursion.c
@@ -1,16 +1,14 @@
 #include "holberton.h"
 
 /**
- * _puts_rev_recursion - print a string in reverse
+ * _print_rev_recursion - print a string in reverse
  * @s: pointer to source string
  */
-void _puts_rev_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
-		return;
-	while (*s != '\0')
+	if (*s != '\0')
 	{
-		_puts_rev_recursion(s + 1);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 }

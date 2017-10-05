b3b8135cb6e3b1762529eab125133e27ac72aaa2
Yunju Chen <info@yunjuchen.net>
2017-10-05 08:54:30 +0000

    2nd commit

--- 0x07-recursion/7-is_palindrome.c
+++ 0x07-recursion/7-is_palindrome.c
@@ -1,46 +1,50 @@
 #include "holberton.h"
 
-int _strlen_recursion(char *);
+int _strlen(char *);
 int _checker(char *, char *);
 
 /**
- * _is_palindrome - return palindrome result
+ * is_palindrome - return palindrome result
  * @s: string to be checked
  * Return: 1 - is palindrome
  *         0 - not palindrome
  */
-int _is_palindrome(char *s)
+int is_palindrome(char *s)
 {
-	if (*s != s[n - 1])
+	if (_checker(s, s + _strlen(s) - 1) == 0)
 		return (0);
-	*s == s[n - 1]
-	return (1);
-
-	if ()
-	
-	if ()
+	else if (_strlen(s) <= 1)
+		return (0);
+	return (_checker(s, s + _strlen(s) - 1));
 }
 
 /**
  * _checker - check if letters are the same
  * @s: begining of string
- * @p: string length
+ * @p: end of string
+ * @len: string lenght
  * Return: 1 - is the same
  *         0 - not the same
  */
-int _checker(char *s, int n)
+int _checker(char *s, char *p)
 {
-
+	if (*s != *p)
+		return (0);
+	else
+	{
+		_checker(s + 1, p - 1);
+		return (1);
+	}
 }
 
 /**
- * _strlen_recursion - return length of a string
+ * _strlen - return length of a string
  * @s: pointer to source string
  * Return: string length
  */
-int _strlen_recursion(char *s)
+int _strlen(char *s)
 {
 	if (*s == '\0')
 		return (0);
-	return (1 + _strlen_recursion(s + 1));
+	return (1 + _strlen(s + 1));
 }

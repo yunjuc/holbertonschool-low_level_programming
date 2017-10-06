6c67584c7ec8dc7426f1bbec494ef541814e6783
Yunju Chen <info@yunjuchen.net>
2017-10-06 22:13:35 +0000

    first commit

---
+++ 0x09-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print the number of arguments
+ * Description: print number of arguments passed to main
+ * Return: 0
+ */
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}

836142135d82453dfd66d5f7f2c764f84b493b68
Yunju Chen <info@yunjuchen.net>
2017-10-06 04:48:30 +0000

    test edge case

--- 0x07-recursion/7-is_palindrome.c
+++ 0x07-recursion/7-is_palindrome.c
@@ -1,36 +1,32 @@
 #include "holberton.h"
 
 int _strlen(char *);
-int _checker(char *, char *);
+int checker(char *, char *, int);
 
 /**
- * is_palindrome - return palindrome result
+ * is_palindrome - check if a string is palindrome
  * @s: string to be checked
  * Return: 1 - is palindrome
  *         0 - not palindrome
  */
 int is_palindrome(char *s)
 {
-	if (_checker(s, s + _strlen(s) - 1) == 0)
-		return (0);
-	else if (_strlen(s) <= 1)
-		return (0);
-	return (_checker(s, s + _strlen(s) - 1));
+	return (checker(s, s + _strlen(s) - 1, _strlen(s)));
 }
 
 /**
- * _checker - check if letters are the same
+ * checker - check if first and last letters are the same
  * @s: begining of string
  * @p: end of string
  * @len: string lenght
  * Return: 1 - is the same
  *         0 - not the same
  */
-int _checker(char *s, char *p)
+int checker(char *s, char *p, int len)
 {
-	if (*s != *p)
+	if (*s != *p || len == 0)
 		return (0);
-	_checker(s + 1, p - 1);
+	checker(s + 1, p - 1, len);
 	return (1);
 }
 

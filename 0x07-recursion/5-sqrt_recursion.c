9622b68317c4832c0d01dd88253e54d91b109f84
Yunju Chen <info@yunjuchen.net>
2017-10-06 04:32:15 +0000

    2nd commit

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -1,5 +1,7 @@
 #include "holberton.h"
 
+int checker(int, int);
+
 /**
  * _sqrt_recursion - return square root of a number
  * @n: number to be chcked
@@ -7,7 +9,24 @@
  */
 int _sqrt_recursion(int n)
 {
+	int base = 1;
+
 	if (n < 0)
 		return (-1);
-	return ();
+	return (checker(n, base));
+}
+
+/**
+ * checker - check if there is square root
+ * @n: number to be chcked
+ * @base: number to compare
+ * Return: square root or -1
+ */
+int checker(int n, int base)
+{
+	if (n / base == base && n % base == 0)
+		return (base);
+	else if  (n / base < base)
+		return (-1);
+	return (checker(n, base + 1));
 }

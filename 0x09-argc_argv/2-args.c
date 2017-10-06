fd42a41c0e0af88cd1cb811a28e9b0639de04492
Yunju Chen <info@yunjuchen.net>
2017-10-06 23:08:34 +0000

    first commit

---
+++ 0x09-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print the argument
+ * Description: print the contents of arguments passed to main
+ * @argc: number count of argument
+ * @argv: arguments in string
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}

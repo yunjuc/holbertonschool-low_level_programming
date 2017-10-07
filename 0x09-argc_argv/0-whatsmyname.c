35c7a8c46c0a7c4745aeaaf2f6134c6102bf8523
Yunju Chen <info@yunjuchen.net>
2017-10-07 02:15:40 +0000

    remove * from *argv

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * Description: print name of current program
  * Return: 0
  */
-int main(int argc __attribute__ ((unused)), char **argv)
+int main(int argc __attribute__ ((unused)), char *argv)
 {
 	printf("%s\n", argv[0]);
 	return (0);

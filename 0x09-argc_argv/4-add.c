4352c2527bc9f34759e155e4d3ab9e6a6d1538a6
Yunju Chen <info@yunjuchen.net>
2017-10-08 16:48:32 +0000

    2nd commit

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -13,18 +13,19 @@ int main(int argc, char **argv)
 	int i, sum = 0;
 
 	if (argc == 1)
-	{
 		printf("0\n");
-	}
-	else if (argc == 0)
-	{
-		printf("Error\n");
-		return (1);
-	}
 	else
 	{
-		for (i = 1; i <= argc; i++)
-			sum = sum + atoi(argv[i]);
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]) == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+				sum = sum + atoi(argv[i]);
+		}
 		printf("%d\n", sum);
 	}
 	return (0);

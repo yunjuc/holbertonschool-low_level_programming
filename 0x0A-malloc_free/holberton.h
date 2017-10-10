d9c166658a3b438157ac5727127c123d65e03bda
Yunju Chen <info@yunjuchen.net>
2017-10-10 20:22:58 +0000

    first commit

---
+++ 0x0A-malloc_free/holberton.h
@@ -0,0 +1,12 @@
+fndef Holberton_h
+#define Holberton_h
+
+int _putchar(char);
+char *create_array(unsigned int, char);
+char *_strdup(char *);
+char *str_concat(char *, char *);
+int **alloc_grid(int, int);
+void free_grid(int **, int);
+char *argstostr(int, char **);
+
+#endif

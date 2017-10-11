31cc001961a74b72fd0c9365c5c2bbcc2be3710c
Yunju Chen <info@yunjuchen.net>
2017-10-11 05:55:24 +0000

    first commit

---
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - print a grid
+ * @width: width of the grid
+ * @height: height of the grid
+ * Return: a grid initialized to 0
+ *         NULL - failure, width or height = 0
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **grid;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width *sizeof(int));
+		if (grid[i] == NULL)
+			return (NULL);
+	}
+
+	for (i = 0; i < height ; i++)
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	return (grid);
+}

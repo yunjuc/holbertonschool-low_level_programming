389c0750c4e69eba20ea45926a3c0208e8cc6797
Yunju Chen <info@yunjuchen.net>
2017-10-11 08:44:42 +0000

    use 2D array to access argv[i][j])

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - add numbers
@@ -10,7 +11,7 @@
  */
 int main(int argc, char **argv)
 {
-	int i, sum = 0;
+	int i, j, sum = 0;
 
 	if (argc == 1)
 		printf("0\n");
@@ -18,13 +19,15 @@ int main(int argc, char **argv)
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (atoi(argv[i]) == 0)
+			for (j = 0; argv[i][j] != '\0'; j++)
 			{
-				printf("Error\n");
-				return (1);
+				if (!isdigit(argv[i][j]))
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
-			else
-				sum = sum + atoi(argv[i]);
+			sum = sum + atoi(argv[i]);
 		}
 		printf("%d\n", sum);
 	}

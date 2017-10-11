2ac44fd9534262f718a715c22991931770fb2165
Yunju Chen <info@yunjuchen.net>
2017-10-11 00:26:16 +0000

    first commmit

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - copy a string
+ * @str: string to be copied
+ * Description: copy a string from memeory and return a pointer point to it
+ * Return: pointer to copied string
+ *         NULL - string is NULL, or memory is insufficient
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	int i, length;
+
+	if (str == NULL)
+		return (NULL);
+	for (length = 0; str[length] != '\0'; length++)
+		length = length;
+	ptr = malloc((length + 1) * sizeof(str));
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0 ; str[i] != '\0' ; i++)
+		ptr[i] = str[i];
+	ptr[i] = '\0';
+	return (ptr);
+}

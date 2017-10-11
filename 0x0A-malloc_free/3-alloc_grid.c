aab2b6325774738c9c9198196bdc83a5f41e8937
Yunju Chen <info@yunjuchen.net>
2017-10-11 20:13:36 +0000

    remove free()

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -19,7 +19,6 @@ int **alloc_grid(int width, int height)
 	grid = malloc(height * sizeof(int *));
 	if (grid == NULL)
 	{
-		free(grid);
 		return (NULL);
 	}
 	for (i = 0; i < height; i++)
@@ -27,7 +26,6 @@ int **alloc_grid(int width, int height)
 		grid[i] = malloc(width * sizeof(int));
 		if (grid[i] == NULL)
 		{ 
-			free(grid[i]);
 			return (NULL);
 		}
 	}

203b8417698cb25a909a0a745935b8f25bddd9e0
Yunju Chen <info@yunjuchen.net>
2017-10-11 20:19:25 +0000

    free memory

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -10,7 +10,7 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int i, j, n;
+	int i, j;
 	int **grid;
 
 	if (width <= 0 || height <= 0)
@@ -19,13 +19,15 @@ int **alloc_grid(int width, int height)
 	grid = malloc(height * sizeof(int *));
 	if (grid == NULL)
 	{
+		free(grid);
 		return (NULL);
 	}
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(width * sizeof(int));
 		if (grid[i] == NULL)
-		{ 
+		{
+			free(grid[i]);
 			return (NULL);
 		}
 	}

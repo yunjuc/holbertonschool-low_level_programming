cc07d0d4863ebe09dcfcfcbef64a04a8b4a22c46
Yunju Chen <info@yunjuchen.net>
2017-10-11 22:40:11 +0000

    free all grid[i] memory

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -27,7 +27,9 @@ int **alloc_grid(int width, int height)
 		grid[i] = malloc(width * sizeof(int));
 		if (grid[i] == NULL)
 		{
-			free(grid[i]);
+			for ( ; i >= 0; i--)
+				free(grid[i]);
+			free(grid);
 			return (NULL);
 		}
 	}

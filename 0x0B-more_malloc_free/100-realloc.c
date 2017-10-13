0f6cd563485f18526f139c5e71c51b231fc32b10
Yunju Chen <info@yunjuchen.net>
2017-10-13 20:52:53 +0000

    i < new_size

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -16,19 +16,23 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (old_size == new_size)
 		return (ptr);
-	if (ptr == NULL)
-		p = malloc((old_size + new_size) * sizeof(unsigned int));
-	else if (new_size == 0 && ptr != NULL)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		p = malloc(new_size * sizeof(unsigned int));
+		if (p == NULL)
+			return (NULL);
 	}
 	else
 	{
 		p = malloc(new_size * sizeof(unsigned int));
 		if (p == NULL)
 			return (NULL);
-		for (i = 0; orig[i] != '\0'; i++)
+		for (i = 0; orig[i] != '\0' && i < new_size ; i++)
 			p[i] = orig[i];
 		free(ptr);
 	}

642c2e642697e55173c5a75d3e48bb909dde7aba
Yunju Chen <info@yunjuchen.net>
2017-10-13 19:20:06 +0000

    change strlen(s2) to n

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -20,7 +20,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	ptr = malloc((_strlen(s1) + _strlen(s2) + 1) * sizeof(char));
+	ptr = malloc((_strlen(s1) + n + 1) * sizeof(char));
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)

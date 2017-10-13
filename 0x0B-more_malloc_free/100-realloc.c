62710e7b98dd64f77fa60214a11f8c01618d9951
Yunju Chen <info@yunjuchen.net>
2017-10-13 05:43:20 +0000

    first commit

---
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - reallocate a memory block
+ * @ptr: pointer to old block
+ * @old_size: old memory size
+ * @new_size: new memory size
+ * Return: pointer to new block
+ *         NULL - failure
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p, *orig = (char *)ptr;
+	unsigned int i;
+
+	if (old_size == new_size)
+		return (ptr);
+	else if (ptr == NULL)
+		p = malloc((old_size + new_size) * sizeof(unsigned int));
+	else if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	else
+	{
+		p = malloc(new_size * sizeof(unsigned int));
+		if (p == NULL)
+			return (NULL);
+		for (i = 0; orig[i] != '\0'; i++)
+			p[i] = orig[i];
+		free(ptr);
+	}
+	return (p);
+}

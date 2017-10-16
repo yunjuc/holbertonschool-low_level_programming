13bb05ea986fb3db82f0d252720cfb6dd9f07434
Yunju Chen <info@yunjuchen.net>
2017-10-16 21:44:54 +0000

    add () to enclose argument

--- 0x0C-preprocessor/3-function_like_macro.h
+++ 0x0C-preprocessor/3-function_like_macro.h
@@ -1,7 +1,5 @@
 #ifndef ABS_H
 #define ABS_H
-#define ABS(x) ((x) < 0) ? (-x) : (x)
-/*
-#define ABS(x) if ((x) < 0) {(-x)} else {(x)}
-*/
+#define ABS(x) (((x) < 0) ? (-x) : (x))
+/* #define ABS(x) (if ((x) < 0) {(-x)} else {(x)})*/
 #endif

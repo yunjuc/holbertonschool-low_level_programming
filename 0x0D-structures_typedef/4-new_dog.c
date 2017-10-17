d80259bf724d0e4345727e999e1f162abebea3f3
Yunju Chen <info@yunjuchen.net>
2017-10-17 05:24:21 +0000

    first commit

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -11,6 +11,10 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new;
+	char dog_name, dog_owner;
+
+	dog_name = name;
+	dog_owner = owner;	
 
 	new = malloc(sizeof(dog_t));
 	if (new == NULL)
---
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - free malloc memory
+ * @d: struct dog
+ * Return: 0
+ */
+void free_dog(dog_t *d)
+{
+	free(d);
+}

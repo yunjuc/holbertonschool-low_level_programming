4cd08139d123ba246f11f20e0fbf8e2513dad31b
Yunju Chen <info@yunjuchen.net>
2017-10-17 03:14:10 +0000

    add typedef dog_t

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -7,12 +7,12 @@
  * @age: dog age
  * @owner: dog owner
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 
 void init_dog(struct dog *, char *, float, char *);
 void print_dog(struct dog *);

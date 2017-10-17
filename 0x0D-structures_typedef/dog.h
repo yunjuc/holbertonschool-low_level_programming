27256dfbfbd0e16777315a4c118681e35ddfe633
Yunju Chen <info@yunjuchen.net>
2017-10-17 03:08:44 +0000

    first commit

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - a structure of dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *, char *, float, char *);
+void print_dog(struct dog *);
+dog_t *new_dog(char *, float, char *);
+void free_dog(dog_t *);
+
+#endif

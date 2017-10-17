94bf931f1fef84d6d7ebd3d325fd4691c2d5d203
Yunju Chen <info@yunjuchen.net>
2017-10-17 06:17:04 +0000

    add NULL conditions

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -9,13 +9,17 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-		printf("(nil)\n");
-	else if (d->name == NULL)
+		printf("\n");
+	if (d->name == NULL)
 		printf("Name: (nil)\n");
 	else
-	{
 		printf("Name: %s\n", d->name);
+	if (d->age == 0)
+		printf("Age: (nil)\n");
+	else
 		printf("Age: %f\n", d->age);
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
 		printf("Owner: %s\n", d->owner);
-	}
 }

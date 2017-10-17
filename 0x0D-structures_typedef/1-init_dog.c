d7d72da3779f0a5f8b918eedb71ac3f227fb8b0a
Yunju Chen <info@yunjuchen.net>
2017-10-17 20:11:33 +0000

    add NULL and stdio library

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 
 /**
  * init_dog - initialize variable for type struct dog
@@ -10,6 +11,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL);
+		return;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;

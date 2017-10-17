c1dde1179cf69633bb331e8031bc5d446d4fb255
Yunju Chen <info@yunjuchen.net>
2017-10-17 03:56:16 +0000

    first commit

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize variable for type struct dog
+ * @d: new dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: 0
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}

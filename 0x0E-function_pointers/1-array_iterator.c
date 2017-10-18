1c511712bad0d552c62658a8a4c12b0be7f4468a
Yunju Chen <info@yunjuchen.net>
2017-10-18 20:24:08 +0000

    first commit

---
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * array_iterator - print a name
+ * @int: array pointer
+ * @size: size of array
+ * @action: function pointer
+ * Return: 0
+ */
+void array_iterator(int *array, size_t size, void(*action)(int))
+{
+	unsigned int i = 0;
+
+	if (action == NULL)
+		return;
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}

8ceea45054ecc9ec1063cf132192324d44ec113d
Yunju Chen <info@yunjuchen.net>
2017-10-18 20:08:16 +0000

    include <stddef.h> library

--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -4,7 +4,7 @@
 int _puchar(char);
 void print_name(char *, void (*)(char *));
 #include <stddef.h>
-int int_index(int *, int size, int (*)(int));
 void array_iterator(int *, size_t, void (*)(int));
+int int_index(int *, int size, int (*)(int));
 
 #endif

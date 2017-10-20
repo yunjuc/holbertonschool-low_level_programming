4dd92c6d3aac0e8c7d973d221708fe3afa83e8e5
Yunju Chen <info@yunjuchen.net>
2017-10-20 21:37:59 +0000

    first commit

---
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef variadic_func_h
+#define variadic_func_h
+
+int _putchar(char);
+int sum_them_all(const unsigned int, ...);
+void print_numbers(const char *, const unsigned int, ...);
+void print_strings(const char *, const unsigned int, ...);
+void print_all(const char * const, ...);
+
+#endif

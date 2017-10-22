78eb8ef59f749c0e9c4024b30221b9e18455d27b
Yunju Chen <info@yunjuchen.net>
2017-10-22 20:14:15 +0000

    create *s to hold string

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -13,18 +13,19 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i;
+	char *s;
 
 	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
 	{
-/*
-*		if (va_arg(ap, char *) == NULL)
-*			printf("(nil)");
-*/
-		printf("%s", va_arg(ap, char *));
-		if (i + 1 != n && separator != NULL)
-			printf("%s", separator);
+		s = va_arg(ap, char *);
+		if (s == NULL)
+			printf("(nil)");
+		else
+			printf("%s", s);
+		if (separator != NULL && i + 1 != n)
+		printf("%s", separator);
 	}
 	printf("\n");
 	va_end(ap);

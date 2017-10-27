e5ab15af91bfd481d086aa6e316c91fd4c1a62f8
Yunju Chen <info@yunjuchen.net>
2017-10-27 01:52:11 +0000

    Use printf to print

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -1,8 +1,4 @@
 #include "lists.h"
-#include <unistd.h>
-
-int _putchar(char c);
-void str_print(char *str);
 
 /**
  * print_list - print elements in a list
@@ -15,38 +11,16 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		str_print(h->str);
+/*
+		if (h->str == NULL)
+		{
+			temp->str = "(nil)";
+			temp->len = 0;
+		}
+*/
+		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		size++;
 	}
 	return (size);
-}
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- * str_print - print a string
- * @str: pointer to string
- * Return: 0
- */
-void str_print(char *str)
-{
-	int i;
-	char *s = str;
-
-	if (s == NULL)
-		s = "(nil)";
-	for (i = 0; s[i] != 0; i++)
-		_putchar(s[i]);
-	_putchar('\n');
 }

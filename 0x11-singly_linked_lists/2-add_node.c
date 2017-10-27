126503deec34e46002f0c2d3115afc326cea9455
Yunju Chen <info@yunjuchen.net>
2017-10-27 03:33:42 +0000

    First commit

---
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * add_node - add node to a list
+ * @head: pointer to list head
+ * @str: string to be copied
+ * Return: number of elements
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = *head;
+	*head = new;
+	return (*head);
+}

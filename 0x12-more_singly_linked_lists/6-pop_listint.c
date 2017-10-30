5e2be17debbc3906ebc0a0ddf4712efe7f0ab2e6
Yunju Chen <info@yunjuchen.net>
2017-10-30 20:42:50 +0000

    First commit

---
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * pop_listint - remove head node from a list
+ * @head: pointer to list head
+ * Return: data of head node; 0 if list is empty
+ */
+int pop_listint(listint_t **head)
+{
+	int n = (*head)->n;
+	listint_t *temp = *head;
+
+	if (*head == NULL)
+		return (0);
+	*head = (*head)->next;
+	free(temp);
+	return (n);
+}

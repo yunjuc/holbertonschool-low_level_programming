dae9be48acb4ab6c9c9b9e6bef5ae4d40d3f2a58
Yunju Chen <info@yunjuchen.net>
2017-11-01 05:00:11 +0000

    First commit

---
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * delete_nodeint_at_index - delete node from a list
+ * @head: pointer to list head
+ * @index: node index
+ * Return: 1 if succeeded, -1 if failed
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *temp, *current = *head;
+	unsigned int i = 0;
+
+	if (index == 0)
+	{
+		temp = (*head)->next;
+		*head = temp;
+		free(current);
+	}
+	else
+	{
+		while (i + 1 < index)
+		{
+			if (current == NULL)
+				return (-1);
+			current = current->next;
+			i++;
+		}
+		temp = current->next;
+		current->next = current->next->next;
+		free(temp);
+	}
+	return (1);
+}
+

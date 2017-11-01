1988ce3ed67dbf16ea95eab5937a96ce9e8ea14a
Yunju Chen <info@yunjuchen.net>
2017-11-01 05:02:30 +0000

    Check if *head is NULL

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,6 +12,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *temp, *current = *head;
 	unsigned int i = 0;
 
+	if (*head == NULL)
+		return (-1);
 	if (index == 0)
 	{
 		temp = (*head)->next;

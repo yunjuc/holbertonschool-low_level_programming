032a76e01cbf3d57608f5a59ed27cad380d3610f
Yunju Chen <info@yunjuchen.net>
2017-11-01 20:13:34 +0000

    Fix index = 0 case

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,6 +1,8 @@
 #include "lists.h"
 #include <stdio.h>
 
+listint_t *add_nodeint(listint_t **head, const int n);
+
 /**
  * insert_nodeint_at_index - insert node into list
  * @head: pointer to list head
@@ -10,22 +12,23 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *temp, *new, *current = *head;
+	listint_t *temp, *new, *current;
 	unsigned int i = 0;
 
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 	if (idx == 0)
-	{
-		new->n = n;
-		new->next = (*head)->next;
-		*head = new;
-	}
+		new = add_nodeint(&(*head), n);
+	else if ((head == NULL || *head == NULL) && idx == 0)
+		new = add_nodeint(&(*head), n);
+	else if ((head == NULL || *head == NULL) && idx != 0)
+		return (NULL);
 	else
 	{
 		while (i + 1 < idx)
 		{
+			current = *head;
 			if (current == NULL)
 				return (NULL);
 			current = current->next;
@@ -36,5 +39,24 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new->n = n;
 		new->next = temp;
 	}
+	return (new);
+}
+
+/**
+ * add_nodeint - add node to a list
+ * @head: pointer to list head
+ * @n: data of new node
+ * Return: pointer to new node; NULL if failed
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
 	return (new);
 }

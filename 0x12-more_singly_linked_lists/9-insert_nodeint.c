c79afb9fbc34952cf83a4ec7b54a91f92fdfa918
Yunju Chen <info@yunjuchen.net>
2017-11-01 20:30:58 +0000

    Fix index is out of list

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,27 +18,30 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
-	if (idx == 0)
+	if ((head == NULL || *head == NULL) && idx == 0)
+	{
 		new = add_nodeint(&(*head), n);
-	else if ((head == NULL || *head == NULL) && idx == 0)
-		new = add_nodeint(&(*head), n);
+		return (new);
+	}
 	else if ((head == NULL || *head == NULL) && idx != 0)
 		return (NULL);
-	else
+	if (idx == 0)
 	{
-		while (i + 1 < idx)
-		{
-			current = *head;
-			if (current == NULL)
-				return (NULL);
-			current = current->next;
-			i++;
-		}
-		temp = current->next;
-		current->next = new;
-		new->n = n;
-		new->next = temp;
+		new = add_nodeint(&(*head), n);
+		return (new);
 	}
+	current = *head;
+	while (i + 1 < idx)
+	{
+		if (current == NULL)
+			return (NULL);
+		current = current->next;
+		i++;
+	}
+	temp = current->next;
+	current->next = new;
+	new->n = n;
+	new->next = temp;
 	return (new);
 }
 

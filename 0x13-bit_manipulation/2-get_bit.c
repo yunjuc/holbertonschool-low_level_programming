df3ec162dae8571c462e072daee7ed28fd62ac8a
Yunju Chen <info@yunjuchen.net>
2017-11-02 23:38:09 +0000

    First commit

---
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * get_bit - return value of a given bit
+ * @index: index bit
+ * Return: bit value, -1 if failed
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int mask = 1;
+	int bit;
+
+	mask = mask << index;
+	if ((mask & n) == 0)
+		bit = 0;
+	else
+		bit = 1;
+	return (bit);
+}

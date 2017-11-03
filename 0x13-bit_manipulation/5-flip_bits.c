42e4f8b1700d0ea2b47a633b7e251b383180e77d
Yunju Chen <info@yunjuchen.net>
2017-11-03 05:24:10 +0000

    First commit

---
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * flip_bit - flip bit value from n to m
+ * @n: old number
+ * @m: new number
+ * Return: number of bits flipped to get the new number
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int count = 0;
+	unsigned long int diff = 0;
+
+	diff = n ^ m;
+	while (diff)
+	{
+		if ((diff & 1) == 1)
+			count++;
+		diff = diff >> 1;
+	}
+	return (count);
+}

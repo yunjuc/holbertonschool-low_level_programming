df85b2b26ef22c28e9f8db1056e49e090a511a00
Yunju Chen <info@yunjuchen.net>
2017-11-08 08:35:34 +0000

    Fix betty issue. Change exit() to return()

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -19,7 +19,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL && text_content == NULL)
 		return (-1);
 	else if (text_content == NULL)
-		exit (1);
+		return (1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)

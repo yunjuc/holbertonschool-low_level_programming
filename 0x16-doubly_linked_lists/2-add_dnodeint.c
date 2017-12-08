226b7c86b9f965a6c37990c8469f38cd914e6ab6
Yunju Chen <info@yunjuchen.net>
2017-12-08 20:00:07 +0000

    First commit

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
-		return(NULL);
+		return (NULL);
 
 	new->n = n;
 	new->prev = NULL;

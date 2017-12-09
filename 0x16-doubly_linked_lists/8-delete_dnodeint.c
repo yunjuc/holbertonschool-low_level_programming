914867ab6171fa577ea258e1a44d81669999ee95
Yunju Chen <info@yunjuchen.net>
2017-12-09 07:21:16 +0000

    First commit

---
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - delete index node from a list
+ * @head: pointer to list head
+ * @index: node index
+ * Return: 1 when succeeded, -1 when failed
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp = *head;
+	unsigned int count = 0;
+
+	if (head == NULL && *head == NULL)
+		return (-1);
+
+	while (temp->next != NULL)
+	{
+		if (count == index)
+		{
+			if (count == 0)
+			{
+				temp->next->prev = NULL;
+				*head = temp->next;
+			}
+			else
+			{
+				if (temp->next == NULL)
+					temp->prev->next = NULL;
+				else
+					temp->prev->next = temp->next;
+			}
+			free(temp);
+			return (1);
+		}
+		temp = temp->next;
+		count++;
+	}
+	return (-1);
+}

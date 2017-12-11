9f12e9346687adb331e4c18b0eb68b9f4065321c
Yunju Chen <info@yunjuchen.net>
2017-12-11 06:03:57 +0000

    Add return last node

--- 0x16-doubly_linked_lists/5-get_dnodeint.c
+++ 0x16-doubly_linked_lists/5-get_dnodeint.c
@@ -5,7 +5,7 @@
  * @head: pointer to list head
  * @index: node index
  * Return: pointer to index node, or NULL if node doesn't exist
-*/ 
+ */ 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *node = head;

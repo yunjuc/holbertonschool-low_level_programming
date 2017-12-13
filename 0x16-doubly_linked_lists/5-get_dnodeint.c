b47c0e09e06f74afbb4087c3cc8ce63b54bf9387
Yunju Chen <info@yunjuchen.net>
2017-12-13 07:02:53 +0000

    Fix betty issue

--- 0x16-doubly_linked_lists/5-get_dnodeint.c
+++ 0x16-doubly_linked_lists/5-get_dnodeint.c
@@ -5,7 +5,7 @@
  * @head: pointer to list head
  * @index: node index
  * Return: pointer to index node, or NULL if node doesn't exist
- */ 
+ */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *node = head;
@@ -20,6 +20,6 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 			return (node);
 		node = node->next;
 		count++;
-	}	
+	}
 	return (NULL);
 }

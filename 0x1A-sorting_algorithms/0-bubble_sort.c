8e1b259e04d37d7d70398a984a2481c18829743d
Lee Gaines <eightlimbed@gmail.com>
2018-01-26 01:17:14 +0000

    adds bubble_sort and big O info

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -7,6 +7,22 @@
  */
 void bubble_sort(int *array, size_t size)
 {
+	size_t i, j;
+	int temp;
 
-//test
+	if (array == NULL || size == 0)
+		return;
+	for (i = size - 1; i > 0; i--)
+	{
+		for (j = 0; j < i; j++)
+		{
+			if (array[j] > array[j+1])
+			{
+				temp = array[j+1];
+				array[j+1] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+		}
+	}
 }

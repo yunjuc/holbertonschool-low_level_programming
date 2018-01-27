2373be4fc04926632d3a024076e7aa781151f722
Lee Gaines <eightlimbed@gmail.com>
2018-01-27 22:35:28 +0000

    adds selection sort and updates header

---
+++ 0x1A-sorting_algorithms/2-selection_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * selection_sort: selection sort
+ * @array: pointer to an array
+ * @size: size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, n;
+	int temp;
+
+	n = 0;
+	while (n < size)
+	{
+		i = n, j = n;
+		while (i < size)
+		{
+			if (array[i] < array[j])
+				j = i;
+			i++;
+		}
+		if (n != j)
+		{
+			temp = array[n];
+			array[n] = array[j];
+			array[j] = temp;
+			print_array(array, size);
+		}
+		n++;
+	}
+}

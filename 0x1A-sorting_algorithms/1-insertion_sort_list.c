3628001b51cf36724a2d632843236f7bd5d3cf1d
Yunju Chen <info@yunjuchen.net>
2018-01-30 20:16:13 +0000

    inseration v2

--- 0x1A-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -2,82 +2,40 @@
 #include <stdio.h>
 
 /**
- * swap: swaps two nodes
- * @a: 1st node
- * @b: 2nd node
- *
- * Return: void
- */
-void swap(listint_t **list, listint_t *a, listint_t *b)
-{
-	if (a == NULL || b == NULL || list == NULL || *list == NULL)
-		return;
-
-	if (b->prev)
-		b->prev->next = a;
-	a->prev = b->prev;
-	b->next = a->next;
-	if (a->next)
-		a->next->prev = b;
-	a->next = b;
-	b->prev = a;
-	if (a->prev == NULL)
-		*list = a;
-	print_list(*list);
-}
-/**
  * insertion_sort: bubble sort algorithm
  * @list: pointer to a doubly linked link
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *left_head, *right_head, *temp1, *temp2;
-	int flag = 1;
+	listint_t *head, *swap, *temp;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	left_head = *list;
-	right_head = (*list)->next;
-
-	while (right_head != NULL)
+	head = (*list)->next;
+	while (head)
 	{
-		if (left_head->n > right_head->n)
+		if (head->n < head->prev->n)
 		{
-			temp1 = right_head;
-			right_head = right_head->next;
-			swap(list, temp1, left_head);
-			while (temp1->prev && temp1->n < temp1->prev->n)
+			swap = head;
+			while (swap->prev && swap->n < swap->prev->n)
 			{
-				temp2 = temp1->prev;
-				swap(list, temp1, temp2);
+				temp = swap->prev;
+				if (temp->prev)
+					temp->prev->next = swap;
+				swap->prev = temp->prev;
+				temp->next = swap->next;
+				if (swap->next)
+					swap->next->prev = temp;
+				swap->next = temp;
+				temp->prev = swap;
+				if (swap->prev == NULL)
+					*list = swap;
+				print_list(*list);	
 			}
-			if (right_head && left_head->n > right_head->n)
-			{
-				temp1 = right_head;
-				right_head = right_head->next;
-				swap(list, temp1, left_head);
-				while (temp1->n < temp1->prev->n)
-				{
-					temp2 = temp1->prev;
-					swap(list, temp1, temp2);
-					if (temp1 == *list)
-					{
-						flag = 0;
-						break;
-					}
-					flag = 1;
-				}
-			}
+			
 		}
-		if (right_head)
-		{
-			if (flag && right_head->next)
-			{
-				left_head = left_head->next;
-				right_head = right_head->next;
-			}
-		}
+		head = head->next;
 	}
 }
 

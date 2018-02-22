9070e2b63715853a556c1735be51c2353feac51f
Yunju Chen <info@yunjuchen.net>
2018-02-22 07:50:39 +0000

    First commit

---
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''Island perimeter module'''
+
+
+def island_perimeter(grid):
+    '''Island perimeter'''
+    p = 0
+    for i in range(len(grid)):
+        for j in range(len(grid)):
+            if grid[i][j] == 1:
+                p += 4
+                if i > 0 and grid[i-1][j] == 1:
+                    p -= 2
+                if j > 0 and grid[i][j-1] == 1:
+                    p -= 2
+    return p

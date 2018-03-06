7bb99cdefbe3f1eaad93b2cc657e335c5dbc16f2
Yunju Chen <info@yunjuchen.net>
2018-03-06 19:05:06 +0000

    First commit

---
+++ 0x1C-binary_trees/4-binary_tree_is_leaf.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * binary_tree_is_leaf - check if a node is leaf
+ * @parent: pointer to the node to check
+ * Return: 1 if a node, 0 otherwise
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	else if (node->left != NULL)
+		return (0);
+	else if (node->right != NULL)
+		return (0);
+	else
+		return (1);	
+}
---
+++ 0x1C-binary_trees/5-binary_tree_is_root.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * binary_tree_is_leaf - check if a node is root
+ * @parent: pointer to the node to check
+ * Return: 1 if a node, 0 otherwise
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	else if (node->parent == NULL)
+		return (1);
+	else
+		return (0);	
+}

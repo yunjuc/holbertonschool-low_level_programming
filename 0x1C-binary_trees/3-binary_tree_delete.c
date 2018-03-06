fabe6478b30fe1e61334e8487948fc0bfc0942ee
Yunju Chen <info@yunjuchen.net>
2018-03-06 18:46:31 +0000

    First commit

---
+++ 0x1C-binary_trees/3-binary_tree_delete.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * binary_tree_delete - delete a tree
+ * @parent: pointer to tree root note
+ * Return: none
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);	
+
+	free(tree);
+}

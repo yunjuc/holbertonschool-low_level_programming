c232d9dd82a9d85c8946f9922ee6dcfb90af5c31
Yunju Chen <info@yunjuchen.net>
2018-03-08 19:21:08 +0000

    Update comment

--- 0x1C-binary_trees/18-binary_tree_uncle.c
+++ 0x1C-binary_trees/18-binary_tree_uncle.c
@@ -1,13 +1,12 @@
 #include "binary_trees.h"
 
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+
 /**
  * binary_tree_uncle - a function that finds the uncle of a node
  * @node: pointer to the node to find the uncle
  * Return: pointer to the uncle node
  */
-
-binary_tree_t *binary_tree_sibling(binary_tree_t *node);
-
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (node == NULL)

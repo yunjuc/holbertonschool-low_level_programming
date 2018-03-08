e5ec5dd3b77c0aabf614d9fb0541463acf46a6f0
Yunju Chen <info@yunjuchen.net>
2018-03-08 19:37:17 +0000

    Update header

--- 0x1C-binary_trees/binary_trees.h
+++ 0x1C-binary_trees/binary_trees.h
@@ -17,9 +17,8 @@ typedef struct binary_tree_s
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-};
+} binary_tree_t;
 
-typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 
 void binary_tree_print(const binary_tree_t *);

015248ae92c85303bf9eef3691dfb3bdb0db7c0f
Yunju Chen <info@yunjuchen.net>
2018-03-09 18:05:21 +0000

    Change logic

--- 0x1C-binary_trees/15-binary_tree_is_full.c
+++ 0x1C-binary_trees/15-binary_tree_is_full.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
 /**
  * binary_tree_is_full - check if a tree is full
  * @tree: root node of the tree to check
@@ -12,8 +11,10 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	if (binary_tree_is_full(tree->left) == binary_tree_is_full(tree->right))
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	else
-		return (0);
+	if ((tree->left) && (tree->right))
+		return (binary_tree_is_full(tree->left) &&
+			binary_tree_is_full(tree->right));
+	return (0);
 }

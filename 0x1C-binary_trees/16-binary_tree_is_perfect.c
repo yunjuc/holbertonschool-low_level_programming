9bb2a522a4500596d8a6c0009a049e1c3e8b3d82
shannonanderson77 <shannonanderson77@gmail.com>
2018-03-09 22:09:07 +0000

    add check for node being a leaf

--- 0x1C-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1C-binary_trees/16-binary_tree_is_perfect.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int tree_height(const binary_tree_t *node);
@@ -17,8 +18,27 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	if (binary_tree_is_full(tree) == 1 && binary_tree_balance(tree) == 0)
 		return (1);
+	if (binary_tree_is_leaf(tree) == 1)
+		return (1);
 	else
 		return (0);
+}
+
+/**
+ * binary_tree_is_leaf - check if a node is leaf
+ * @node: pointer to the node to check
+ * Return: 1 if a node, 0 otherwise
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	else if (node->left != NULL)
+		return (0);
+	else if (node->right != NULL)
+		return (0);
+	else
+		return (1);
 }
 
 /**

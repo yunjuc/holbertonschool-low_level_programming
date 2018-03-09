6bce33b3737367c50166f9b0b68de2fb4c37bda7
Yunju Chen <info@yunjuchen.net>
2018-03-09 18:05:59 +0000

    Updat binary_tree_is_full

--- 0x1C-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1C-binary_trees/16-binary_tree_is_perfect.c
@@ -30,10 +30,12 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	if (binary_tree_is_full(tree->left) == binary_tree_is_full(tree->right))
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	else
-		return (0);
+	if ((tree->left) && (tree->right))
+		return (binary_tree_is_full(tree->left) &&
+			binary_tree_is_full(tree->right));
+	return (0);
 }
 
 /**

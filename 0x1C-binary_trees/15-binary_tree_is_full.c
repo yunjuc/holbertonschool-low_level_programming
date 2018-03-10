58710e778802977b939297945333c022b0897327
Yunju Chen <info@yunjuchen.net>
2018-03-10 02:47:05 +0000

    Add checking balance

--- 0x1C-binary_trees/15-binary_tree_is_full.c
+++ 0x1C-binary_trees/15-binary_tree_is_full.c
@@ -16,7 +16,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	if ((tree->left) && (tree->right))
+	if ((tree->left) && (tree->right) && binary_tree_balance(tree) == 0)
 		return (binary_tree_is_full(tree->left) &&
 			binary_tree_is_full(tree->right));
 	return (0);

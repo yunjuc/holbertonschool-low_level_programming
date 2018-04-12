5492b725c92d8e90953d8bb28bcb2f154be6943c
Yunju Chen <info@yunjuchen.net>
2018-04-12 01:17:42 +0000

    Add header

---
+++ 0x1D-search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef _SEARCH_ALGO_H_
+#define _SEARCH_ALGO_H_
+
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
